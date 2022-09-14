e397d99cacaa66e14c558d30b6d17de1c1cc0ba3
Sezer Ökmen <szrokmn34@gmail.com>
2022-09-14 12:45:43 +0300

    sellector finished

--- javaScript.js
+++ javaScript.js
@@ -1,3 +1,7 @@
-
+// Selectors
+const langInput = document.querySelector(".lang-input");
+const addBtn = document.querySelector("#add-btn");
+const deleteBtn = document.querySelector("#delete-btn");
+const langList = document.getElementById("lang-list");
    
 
